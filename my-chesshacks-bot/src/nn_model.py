e27c97f7b9c3ee3822f0a28ce348c2f1afcca29f
Nelson Kang <Nelsonkang7@gmail.com>
2026-06-19 15:51:56 -0400

    fix:  fixed dead layers 3&4 in Neural Net

--- my-chesshacks-bot/src/nn_model.py
+++ my-chesshacks-bot/src/nn_model.py
@@ -31,6 +31,8 @@ class ValueNet(nn.Module):
     def forward(self, x):
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
         x = x.view(x.size(0), -1)
         x = F.relu(self.fc1(x))
         return self.fc2(x)
